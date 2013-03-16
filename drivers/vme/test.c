f4a2062be4be4461e3e1dc1c1260e4a839f5f6c1
nam <nam@daq_chamber>
2013-03-16 17:38:43 +0900

    Triggering from pulser on V1718 works now, output1 is used. TIL registers of controller can be accessed by CAENVME_Readreg or CAENVME_writereg

--- drivers/vme/test.c
+++ drivers/vme/test.c
@@ -15,6 +15,7 @@
 #include "v1718.h"
 #include "v792n.h"
 #define V792_BASE  0x54320000
+void showbits(uint32_t data);
 
 int main(int argc, char** argv)
 {
@@ -39,30 +40,64 @@ int main(int argc, char** argv)
 	usleep(100);
 
 	v792n_SoftReset(myvme,V792_BASE);
-	v792n_Setup(myvme,V792_BASE,2); //enable empty events, over range & under thr.
-	v792n_OnlineSet(myvme, V792_BASE);
+	/*v792n_Setup(myvme,V792_BASE,2); //enable empty events, over range & under thr.*/
+	//v792n_OnlineSet(myvme, V792_BASE);
 	v792n_Status(myvme,V792_BASE);
-	/*v1718_PulserConfSet(myvme,v1718_pulserA,1000,200,0);*/
-	//CAENVME_SetOutputConf(BHandle,cvOutput0,cvDirect,cvActiveHigh,cvMiscSignals);
-  //CAENVME_SetPulserConf(BHandle,
-			//cvPulserA,3,3 ,cvUnit25ns,1,cvManualSW,cvInputSrc1);
-	//CVTimeUnits unit;
-	//CVIOSources io,reset;
-	//unsigned char per,wid,no;
-	//CAENVME_GetPulserConf(BHandle,v1718_pulserA,&per,&wid,&unit,&no,&io,&reset);
-	//printf("pulser A: %d %d %d %d %d %d \n",per,wid,unit,no,io,reset);
-	/*v1718_PulserStart(myvme,v1718_pulserA);*/
-	//status = CAENVME_StartPulser(BHandle,cvPulserA);
-	//CAENVME_DecodeError(status);
-	//printf("%d\n",status);
-	//usleep(100);
-	/*v792n_Status(myvme,V792_BASE);*/
-	//WORD buff16;
+	sleep(5);
+	/*CAENVME_SetOutputConf(BHandle,cvOutput0,cvDirect,cvActiveHigh,cvManualSW);*/
+	v1718_PulserConfSet(myvme,v1718_pulserA,1000,200,1);
+	CVTimeUnits unit;
+	CVIOSources io,reset;
+	unsigned char per,wid,no;
+	WORD buff16;
+	CAENVME_GetPulserConf(BHandle,v1718_pulserA,&per,&wid,&unit,&no,&io,&reset);
+	printf("pulser A: %d %d %d %d %d %d \n",per,wid,unit,no,io,reset);
+	v1718_PulserStart(myvme,v1718_pulserA);
+	/*CAENVME_ReadRegister(BHandle,cvPulserA0,&buff16);*/
+	/*showbits((uint32_t)buff16);*/
+	//usleep(10000000);
+	/*v1718_PulserStop(myvme,v1718_pulserA);*/
+	v792n_Status(myvme,V792_BASE);
 	//buff16 = v792n_Read16(myvme,V792_BASE,0x1000);
-	//printf("%X\n",buff16);
 
 	// Close
 	CAENVME_End(BHandle);
 	return 0;
+}
+void showbits(uint32_t data)
+{
+
+    int ii;
+    for (ii=31;ii>=28;ii--)
+        (data&(1<<ii))?putchar('1'):putchar('0');
+    putchar(' ');
+
+    for (ii=27;ii>=24;ii--) 
+        (data&(1<<ii))?putchar('1'):putchar('0');
+    putchar(' ');
+
+    for (ii=23;ii>=20;ii--) 
+        (data&(1<<ii))?putchar('1'):putchar('0');
+    putchar(' ');
+
+    for (ii=19;ii>=16;ii--) 
+        (data&(1<<ii))?putchar('1'):putchar('0');
+    putchar(' ');
+
+    for (ii=15;ii>=12;ii--) 
+        (data&(1<<ii))?putchar('1'):putchar('0');
+    putchar(' ');
+
+    for (ii=11;ii>=8;ii--) 
+        (data&(1<<ii))?putchar('1'):putchar('0');
+    putchar(' ');
+
+    for (ii=7;ii>=4;ii--) 
+        (data&(1<<ii))?putchar('1'):putchar('0');
+    putchar(' ');
+
+    for (ii=3;ii>=0;ii--) 
+        (data&(1<<ii))?putchar('1'):putchar('0');
+    putchar(' ');
 }
 
